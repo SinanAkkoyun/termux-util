8b0ccb538e3a6e4feac83d574bf0091f2cb9a942
SinanAkkoyun <43215895+SinanAkkoyun@users.noreply.github.com>
2019-10-02 15:02:35 +0200

    Create install-blender.sh

---
+++ install-blender.sh
@@ -0,0 +1,7 @@
+cd /usr/local
+rm -r blender-2.80-linux-glibc217-x86_64
+wget https://ftp.halifax.rwth-aachen.de/blender/release/Blender2.80/blender-2.80-linux-glibc217-x86_64.tar.bz2
+tar xjf blender-2.80*
+rm blender-2.80-linux-glibc217-x86_64.tar.bz2
+cd
+echo "Installation of Blender 2.80 done!"
