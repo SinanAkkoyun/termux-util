736bb9ed494f0126cf556019d901ddafd623d31c
SinanAkkoyun <43215895+SinanAkkoyun@users.noreply.github.com>
2019-10-03 04:15:00 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 apt update
 apt upgrade -y
 apt dist-upgrade -y
-apt install openssh-server apache2 vim tmux apache2 -y
+apt install openssh-server python3 python3-pip blender vim tmux apache2 -y
 #vnc
