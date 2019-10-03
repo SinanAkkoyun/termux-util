8ea8bd42e14eed3a91c12d35f3c30cbe6e44585d
SinanAkkoyun <43215895+SinanAkkoyun@users.noreply.github.com>
2019-10-03 03:21:41 +0200

    Update install-blender.sh

--- install-blender.sh
+++ install-blender.sh
@@ -1,10 +1,9 @@
 #ubuntu-fs/root/
-cd /usr/local
-rm -r blender-2.80-linux-glibc217-x86_64
-wget https://ftp.halifax.rwth-aachen.de/blender/release/Blender2.80/blender-2.80-linux-glibc217-x86_64.tar.bz2
+cd /usr/local                                                                                   rm -r blender-2.80-linux-glibc217-x86_64                                                        wget https://ftp.halifax.rwth-aachen.de/blender/release/Blender2.80/blender-2.80-linux-glibc217-x86_64.tar.bz2
 mkdir blender
 tar xjf blender-2.80* -C blender
-mv blender/blender-2.80* blender/
+mv blender/blender-2.80-linux-glibc217-x86_64/* blender/
 rm blender-2.80-linux-glibc217-x86_64.tar.bz2
+rm -r blender/blender-2.80*
 cd
 echo "Installation of Blender 2.80 done!"
