3a3749a0d225cc40ee97b53b1368b70723f8a6fe
SinanAkkoyun <43215895+SinanAkkoyun@users.noreply.github.com>
2019-10-05 12:51:49 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ apt upgrade -y
 apt dist-upgrade -y
 apt install openssh-server python3 python3-pip blender vim tmux apache2 -y
 #vnc
+
+add-apt-repository ppa:openjdk-r/ppa
+apt update
+apt install openjdk-8-jdk -y
